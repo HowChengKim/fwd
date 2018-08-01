f658d9ac8193a8eb145310675b2aaa485e7a9048
how <hao.3248@163.com>
2018-08-01 11:47:42 +0800

    完成 智能推荐 静态页面

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Search.styl';
+import Service from '../../components/Service'
 import {Field} from 'amazeui-touch'
 
 const Selects = (
@@ -8,6 +9,12 @@ const Selects = (
     <option value="high">高级搜索</option>
   </Field>
 );
+const dataList = [
+  {url:'http://s.amazeui.org/media/i/demos/bing-2.jpg',name:'李四',account:'86451984919919',count:100,link:'http://www.baidu.com'},
+  {url:'http://s.amazeui.org/media/i/demos/bing-2.jpg',name:'李四',account:'86451984919919',count:100,link:'http://www.baidu.com'},
+  {url:'http://s.amazeui.org/media/i/demos/bing-2.jpg',name:'李四',account:'86451984919919',count:100,link:'http://www.baidu.com'},
+  {url:'http://s.amazeui.org/media/i/demos/bing-2.jpg',name:'李四',account:'86451984919919',count:100,link:'http://www.baidu.com'}
+];
 class Search extends Component {
   constructor(props) {
     super(props);
@@ -24,6 +31,24 @@ class Search extends Component {
       </span>
     );
   }
+  initResult(){
+    return dataList.map((o,i)=>{
+      return (
+        <div key={i}>
+          <img src={o.url} />
+          <div className="info-box">
+            <span>{o.name}</span>
+            <p>账号：{o.account}</p>
+          </div>
+          <div className="bottom-box">
+            <i className="point"></i>
+            <p>已办理超过 <span>{o.count}</span> 件类似案件</p>
+            <a href={o.link}>案例</a>
+          </div>
+        </div>
+      )
+    });
+  }
   render() {
     return (
       <div className="search-box">
@@ -35,6 +60,28 @@ class Search extends Component {
           <div className="search-history">
             <span>婚姻继承</span><span>侵权纠纷</span><span>交通事故</span><span>工伤赔偿</span><span>医疗纠纷</span><span>债权债务</span><span>劳动仲裁</span>
           </div>
+          <div className="search-condition">
+            <p>限制性检索：</p>
+            <div className="condition">
+              <div>
+                <span>案由：</span>
+                <input type="text" name="案由：" placeholder="不限"/>
+              </div>
+              <div>
+                <span>地区：</span>
+                <select name="地区">
+                  <option value="default">不限</option>
+                  <option value="SZ">深圳</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="search-result">
+          <div className="result-content">
+            {this.initResult()}
+          </div>
+          <Service />
         </div>
       </div>
     );
